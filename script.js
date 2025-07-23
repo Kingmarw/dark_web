2aef4c4a3e8ef5d6781b26ac4b18351c345443ef
Kingmarw <163139467+Kingmarw@users.noreply.github.com>
2025-07-23 11:14:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+// 
 let inp = document.getElementById('inp');
 let btn = document.getElementById('btn');
 let link = document.createElement('a');
 let title = document.createElement('p');
 let j = document.getElementById("j");
-let h = document.createElement('h1')
-let txt2 = document.createTextNode("You are stupid!");
+let h = document.createElement('h1');
+let txt2 = document.createTextNode("You are lose,lol!");
 let txt = document.createTextNode("Click here!");
-let txt1 = document.createTextNode("You are stupid!");
+let txt1 = document.createTextNode("You are lose,lol!");
 let code = document.createElement('h1');
 let hacker = document.createTextNode("0101010111010101010101010");
 let codep = document.createElement('h1');
@@ -14,13 +15,13 @@ let hackerp = document.createTextNode("01010101110101010");
 let codei = document.createElement('h1');
 let hackeri = document.createTextNode("01010101110101010");
 let div = document.getElementById("divaya")
-code.appendChild(hacker)
-codep.appendChild(hackerp)
-codei.appendChild(hackeri)
-h.appendChild(txt2)
-title.appendChild(txt)
+code.appendChild(hacker);
+codep.appendChild(hackerp);
+codei.appendChild(hackeri);
+h.appendChild(txt2);
+title.appendChild(txt);
 link.setAttribute('href', 'stupid.html');
-link.appendChild(title)
+link.appendChild(title);
 link.style.color = '#333';
 h.style.alignItems = 'center';
 h.style.textAlign = 'center';
@@ -36,6 +37,7 @@ codei.style.textAlign = 'center';
 
 function card() {
     let cardContainer = document.createElement('div');
+    cardContainer.setAttribute('class','kingc')
     let container = document.getElementById('container');
     let cardTitle = document.createElement('h1');
     let textitle = document.createTextNode('Dark web');
@@ -47,14 +49,6 @@ function card() {
     cardContainer.appendChild(cardDisc);
     container.appendChild(cardContainer);
     document.body.appendChild(container);
-    cardContainer.style.background = 'rgba(255, 255, 255, 0.5)';
-    cardContainer.style.backdropFilter = 'blur(19px);';
-    cardContainer.style.margin = '10px';
-    cardContainer.style.justifyContent = 'center';
-    cardContainer.style.alignItems = 'center';
-    cardContainer.style.textAlign = 'center';
-    cardContainer.style.borderRadius = '15px';
-    cardContainer.style.padding = '15px';
     container.style.display = 'flex';
     container.style.flexDirection = 'row';
     container.style.justifyContent = 'center';
@@ -65,8 +59,6 @@ function card() {
 for (let i = 0; i < 4; i++) {
     card();
 }
-
-
 
 // conditions
 if (localStorage.length > 0) {
@@ -81,28 +73,18 @@ btn.onclick = function () {
         container.style.display = 'none';
         alert("Ooops! the website has been hacked!");
         function vul(){
-            document.body.appendChild(code)
-            document.body.appendChild(codep)
-            document.body.appendChild(codei)
+            document.body.appendChild(code);
+            document.body.appendChild(codep);
+            document.body.appendChild(codei);
         }
         vul()
     } else if (localStorage.name == "") {
         j.removeChild(txt1);
         div.removeChild(link);
-        div.removeChild(h)
+        div.removeChild(h);
     } else if (localStorage.name != "") {
         j.appendChild(txt1);
         div.appendChild(link);
-        div.appendChild(h)
+        div.appendChild(h);
     }
 }
-
-
-
-
-
-
-
-
-
-
